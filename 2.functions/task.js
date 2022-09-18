ea7496dfbb3183e6ed4e1cfa7d90eae71fd750f4
Maksim Lazarev <felshines@gmail.com>
2022-09-18 14:36:06 +0300

    second homework

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -2,30 +2,66 @@
 function getArrayParams(arr) {
   let min, max, sum, avg;
 
-  // Ваш код
+    min = Infinity;
+    max = -Infinity;
+    sum = 0;
+
+    for (let i = 0; i < arr.length; ++i) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        sum += arr[i];
+    }
+
+    avg = Number((sum / arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+    for (let i = 0; i < arr.length; ++i) {
+        sum += arr[i];
+    }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+    let max = -Infinity;
 
-  // Ваш кода
-  // for ...
+    for (let i = 0; i < arrOfArr.length; ++i) {
+        if (max < func(arrOfArr[i])) {
+            max = func(arrOfArr[i]);
+        }
+    }
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+    let min, max, difference;
+
+    min = Infinity;
+    max = -Infinity;
+
+    for (let i = 0; i < arr.length; ++i) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    difference = Math.abs(max - min);
+
+    return { min: min, max: max, difference: difference };
 }
+
