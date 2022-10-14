4a573eab7623a5e432c92b8fa0535367886c8b45
Maksim Lazarev <felshines@gmail.com>
2022-10-15 00:02:39 +0300

    Decorators finish

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,35 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = {};
+  return(...args) => {
+      const hash = args.join(',');
+      if (hash in cache) {
+          return ("Из кэша: " + cache[hash]);
+      }
+      const result = func(...args);
+      if(cache.length === 5){
+          cache.shift();
+      }
+      cache[hash] = result;
+      return ("Вычисляем: " + result);
+  }
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, delay) {
+     function wrapper(...args) {
+         ++wrapper.count;
+        let timeoutId = null;
+        if (timeoutId === null) {
+            timeoutId = setTimeout(() => {
+                console.log(func(...args));
+                ++wrapper.allCount;
+                }, delay);
+        } else{
+            console.log(func(...args), delay);
+        }
+        wrapper.count = 0;
+        wrapper.allCount = 0;
+        return wrapper;
+    }
+
 }
