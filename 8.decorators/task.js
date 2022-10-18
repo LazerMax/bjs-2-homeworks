4f85b52e409ae94434d1737ad1a9bccd5aff630b
Maksim Lazarev <felshines@gmail.com>
2022-10-18 14:05:00 +0300

    Decorators

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,35 +1,69 @@
 function cachingDecoratorNew(func) {
-  let cache = {};
-  return(...args) => {
-      const hash = args.join(',');
-      if (hash in cache) {
-          return ("Из кэша: " + cache[hash]);
-      }
-      const result = func(...args);
-      if(cache.length === 5){
-          cache.shift();
-      }
-      cache[hash] = result;
-      return ("Вычисляем: " + result);
-  }
+    let cache = {};
+    return (...args) => {
+        const hash = args.join(',');
+        if (hash in cache) {
+            return ("Из кэша: " + cache[hash]);
+        } let newCache = {};
+        if (Object.keys(cache).length === 5) {
+
+
+            const keysToKeep = Object.keys(cache).filter(key => key != Object.keys(cache)[0]);
+            console.log(keysToKeep);
+            keysToKeep.forEach((key) => {
+                newCache[key] = cache[key];
+            });
+            cache = {};
+            keysToKeep.forEach((key) => {
+                cache[key] = newCache[key];
+            });
+        }
+        const result = func(...args);
+        cache[hash] = result;
+        return ("Вычисляем: " + result);
+    }
 }
+/*function cachingDecoratorNew(func) {
+    let cache = [];
+
+    function wrapper(...args) {
+        const hash = args.join(',');
+        let objectInCache = cache.find((item) => item === hash);
+        if (!objectInCache) { // если элемент не найден
+            console.log("Из кэша: " + func(...args));
+            return "Из кэша: " + func(...args);
+        }
+
+        let result = func(...args);
+        cache.push(hash);
+        if (cache.length > 5) {
+        cache.shift();
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;
+    }
+    return wrapper;
+}*/
 
 
 function debounceDecoratorNew(func, delay) {
+    wrapper.count = 0;
+    wrapper.allCount = 0;
      function wrapper(...args) {
-         ++wrapper.count;
+         ++wrapper.allCount;
+         console.log(wrapper.allCount);
         let timeoutId = null;
         if (timeoutId === null) {
             timeoutId = setTimeout(() => {
-                console.log(func(...args));
-                ++wrapper.allCount;
+                func(...args);
+                ++wrapper.count;
                 }, delay);
-        } else{
-            console.log(func(...args), delay);
+            clearTimeout(timeoutId);
+        } else {
+            func(...args);
+            ++wrapper.count;
+            console.log(wrapper.count);
         }
-        wrapper.count = 0;
-        wrapper.allCount = 0;
-        return wrapper;
     }
-
+    return wrapper;
 }
