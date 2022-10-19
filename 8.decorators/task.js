cf011765e6fcc7cd11b698924dd8388b90f789fb
Maksim Lazarev <felshines@gmail.com>
2022-10-19 12:56:06 +0300

    Decorators

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,4 +1,4 @@
-function cachingDecoratorNew(func) {
+/*function cachingDecoratorNew(func) {
     let cache = {};
     return (...args) => {
         const hash = args.join(',');
@@ -6,36 +6,30 @@ function cachingDecoratorNew(func) {
             return ("Из кэша: " + cache[hash]);
         } let newCache = {};
         if (Object.keys(cache).length === 5) {
-
-
-            const keysToKeep = Object.keys(cache).filter(key => key != Object.keys(cache)[0]);
-            console.log(keysToKeep);
-            keysToKeep.forEach((key) => {
-                newCache[key] = cache[key];
-            });
-            cache = {};
-            keysToKeep.forEach((key) => {
-                cache[key] = newCache[key];
-            });
+            delete cache[Object.keys(cache)[0]];
         }
         const result = func(...args);
         cache[hash] = result;
         return ("Вычисляем: " + result);
     }
-}
-/*function cachingDecoratorNew(func) {
+}*/
+
+function cachingDecoratorNew(func) {
     let cache = [];
 
     function wrapper(...args) {
         const hash = args.join(',');
-        let objectInCache = cache.find((item) => item === hash);
-        if (!objectInCache) { // если элемент не найден
-            console.log("Из кэша: " + func(...args));
-            return "Из кэша: " + func(...args);
+        let objectInCache = cache.find((item) => item.hash === hash);
+        if (objectInCache) {
+            return "Из кэша: " + objectInCache.value;
         }
 
         let result = func(...args);
-        cache.push(hash);
+        if(cache.length === 0){
+            cache = [{hash: hash, value: result}];
+        } else {
+            cache.push({hash: hash, value: result});
+        }
         if (cache.length > 5) {
         cache.shift();
         }
@@ -43,27 +37,27 @@ function cachingDecoratorNew(func) {
         return "Вычисляем: " + result;
     }
     return wrapper;
-}*/
+}
 
 
 function debounceDecoratorNew(func, delay) {
     wrapper.count = 0;
     wrapper.allCount = 0;
+    let timeoutId = null;
      function wrapper(...args) {
          ++wrapper.allCount;
          console.log(wrapper.allCount);
-        let timeoutId = null;
         if (timeoutId === null) {
-            timeoutId = setTimeout(() => {
-                func(...args);
-                ++wrapper.count;
-                }, delay);
-            clearTimeout(timeoutId);
-        } else {
             func(...args);
             ++wrapper.count;
             console.log(wrapper.count);
+
         }
+         timeoutId = setTimeout(() => {
+             func(...args);
+             ++wrapper.count;
+         }, delay);
     }
+    clearTimeout(timeoutId);
     return wrapper;
 }
